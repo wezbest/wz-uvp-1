0c790136db698ece9348818e6353332e8fd8b27f
wezbest <110793713+wezbest@users.noreply.github.com>
2024-10-07 17:34:27 +0000

    🫦

--- EXP/scriptz/s1.py
+++ EXP/scriptz/s1.py
@@ -12,11 +12,22 @@
 This program primarly for smelling panties
 """
 
+import requests as rq 
+from rich.console import Console # For console.print
+from rich.panel import Panel # For Panel()
+console = Console() # Standard code to access console
+
 def main() -> None:
     print("Hello from s1.py!")
+    banner()
 
 def banner() -> None:
-    print("Hello from banner!")
+    panel = Panel("""
+    Enjoy StinlySmellySWeaty women
+    """,title="Mistress",subtitle="ToiletSlave", style="Italic", border_style="magenta")
+    # Print the Panel
+    console.print(panel)
+
 
 
 if __name__ == "__main__":
