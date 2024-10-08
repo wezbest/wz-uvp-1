b3225e96373211f1f381e3bf11f314e06d75136d
wezbest <110793713+wezbest@users.noreply.github.com>
2024-10-08 07:47:49 +0000

    🫦

--- WORK1/p5/src/p5/__init__.py
+++ WORK1/p5/src/p5/__init__.py
@@ -1,2 +1,23 @@
+import requests as rq
+from rich import print as rprint  # For rprinting
+from rich.pretty import pprint  # For pretty printing
+from rich.console import Console  # For console.print
+from rich.panel import Panel  # For Panel()
+from rich import inspect
+
+console = Console()  # Standard code to access console
+from rich.traceback import install
+
+install(show_locals=True)
+
+
 def main() -> None:
     print("Hello from p5!")
+    pantybanner()
+
+
+def pantybanner() -> None:
+    u1 = "https://snips.sh/f/3jS3Jr5pPs?r=1"
+    u2 = "https://snips.sh/f/3jS3Jr5pPs"
+    rez = rq.get(u1)
+    print(rez.text)
